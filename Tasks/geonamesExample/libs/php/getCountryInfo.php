a1b33e5bd8600e37343e6db6059d1f8df34e6ac4
ManuK145 <manughotra145@gmail.com>
2023-03-01 12:35:07 +0000

    replaced 'geonames' with 'postalcodes' in $decode

--- Tasks/geonamesExample/libs/php/getCountryInfo.php
+++ Tasks/geonamesExample/libs/php/getCountryInfo.php
@@ -8,7 +8,7 @@
 	$executionStartTime = microtime(true);
 
 	//$url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=' . $_REQUEST['lang'] . '&country=' . $_REQUEST['country'] . '&username=flightltd&style=full';
-	$url = 'http://api.postalcodes.org/postalCodeLookupJSON?formatted=true&postalcode=' . $_REQUEST['postalcode'] . '&country=' . $_REQUEST['country'] . '&username=manpreetKaur&style=full';
+	$url = 'http://api.geonames.org/postalCodeLookupJSON?formatted=true&postalcode=' . $_REQUEST["postalcode"] . '&country=' . $_REQUEST["country"] . '&username=manpreetKaur&style=full';
 	
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -25,7 +25,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
+	$output['data'] = $decode['postalcodes'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
