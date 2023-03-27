ff8bb1d9639420d40c5f4fe35a53c28ef8c5293d
ManuKaur145 <zhac080@live.rhul.ac.uk>
2023-03-27 09:36:56 +0100

    removed comments

--- Project1/php/getCountryBorder.php
+++ Project1/php/getCountryBorder.php
@@ -2,16 +2,12 @@
    $result = file_get_contents("../countryBorders.geo.json");
    $decode = json_decode($result,true);
    $countryBorder;
-   for ($i = 0; $i < count($decode[0]['features']); $i++) {
+   for ($i = 0; $i < count($decode[0]['features']); $i++) 
+   {
        if($decode[0]['features'][$i]['properties']['iso_a2'] == $_REQUEST['iso']){
-           $countryBorder = $decode[0]['features'][$i];//['geometry'];
-		   /*$output['data'] = $countryBorder;
-		   header('Content-Type: application/json; charset=UTF-8');
-		   echo json_encode($output);
-		   exit;*/
+           $countryBorder = $decode[0]['features'][$i];
        }
    }
    $output['data'] = $countryBorder;
-   header('Content-Type: application/json; charset=UTF-8');
    echo json_encode($output); 
 ?>
