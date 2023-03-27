ec4fa219a6080db06ba46dd47d5d736c41a568ca
ManuKaur145 <zhac080@live.rhul.ac.uk>
2023-03-27 09:39:10 +0100

    added some styling for new elements

--- Project1/js/fetchCountriesList.js
+++ Project1/js/fetchCountriesList.js
@@ -10,7 +10,13 @@ fetch(countryBorders).then(
 		response.json().then(function(data){
 			for(let c = 0; c < data[0]['features'].length; c++)
 			{
-				countries.innerHTML += "<option value='"+data[0]['features'][c]['properties']['iso_a2']+"'>"+data[0]['features'][c]['properties']['name']+"</option>";
+				allCountriesCodeName.push(data[0]['features'][c]['properties']['name']+"-"+data[0]['features'][c]['properties']['iso_a2']);
+			}
+			allCountriesCodeName.sort();
+			for(let c = 0; c < allCountriesCodeName.length; c++)
+			{
+				let country = allCountriesCodeName[c].split("-");
+				countries.innerHTML += "<option value='"+country[1]+"'>"+country[0]+"</option>";
 			}
 		});
 	}
